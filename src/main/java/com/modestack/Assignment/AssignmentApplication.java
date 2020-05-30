acbb6ab2ac2b44a7670136845e5cd2077432b26c
sachinSinghania1 <66172251+sachinSinghania1@users.noreply.github.com>
2020-05-30 21:18:48 +0530

    initial commit

---
+++ src/main/java/com/modestack/Assignment/AssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.modestack.Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+//@EnableResourceServer
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
